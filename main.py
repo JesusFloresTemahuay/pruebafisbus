30197ee5f75cc8859719bc480f624421454552a7
jesusflorestemahuay <jflorest1993@gmail.com>
2025-05-26 13:11:42 -0600

    Nueva propuesta del algoritmo

--- main.py
+++ main.py
@@ -4,16 +4,19 @@ fiz = "FiZZ"
 buz = "Buzz"
 salida = ""
 for counter in range(1, 1000):
-    div3 = counter % 3 == 0
-    div5 = counter % 5 == 0
-    if(div3 or div5):
-        if(div3):
-            salida = salida + fiz
-        if(div5):
-            salida = salida + buz
+    
+    is_div3 = counter % 3 == 0
+    is_div5 = counter % 5 == 0
+
+    if(is_div3):
+        salida = salida + fiz
+    if(is_div5):
+        salida = salida + buz
+    
+    if(len(salida) > 0):
         print(salida)
     else:
         print(counter)
+
     salida = ""
         
-
